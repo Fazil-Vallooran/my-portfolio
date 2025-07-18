f581d1dd3d74a67a22282fc54a03829698b76d84
Fazil-Vallooran <sigma.ophiuchus@gmail.com>
2025-07-18 23:21:07 +0530

    a

--- src/Components/landing.jsx
+++ src/Components/landing.jsx
@@ -4,7 +4,7 @@ import Particles from "./particles";
 
 const Landing = () => {
   return (
-    <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black" style={{position: "relative"}}>
+    <div className="flex flex-col items-center justify-center h-screen w-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black" style={{position: "relative"}}>
       <Particles />
       <div className="hidden w-screen h-px animate-fade-glow md:block bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />      
       <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text">
