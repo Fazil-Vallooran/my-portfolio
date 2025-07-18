f194f61ee8dbe67263fe5789f709b3120f9a762f
Fazil-Vallooran <sigma.ophiuchus@gmail.com>
2025-07-19 03:57:10 +0530

    removed unused things

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -20,7 +20,6 @@ const RootLayout = ({ children }) => (
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-      <link rel="manifest" href="/site.webmanifest" />
     </head>
     <body className={`${inter.className} h-full min-h-screen bg-gradient-to-b from-[#0a1833] to-[#142042] text-[#bfcbe6]`}>
       <Particles quantity={80} />
